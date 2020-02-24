64258655c8951f7b6d465de8877daa90ffe9d7a3
astroboi <terry007x@korea.ac.kr>
2020-02-24 19:02:39 +0900

    output.txt 불필요

--- Main.py
+++ Main.py
@@ -10,21 +10,22 @@ df = pd.read_excel(EXCEL_PATH, sheet_name=SHEET_NAME)
 
 with open(INPUT_PATH,"a") as f:
     i = 1
+    f.write('Target number	34 bp target sequence (4 bp + PAM + 23 bp protospacer + 3 bp)		"Chromatin accessibility (1= DNase I hypersensitive sites, 0 = Dnase I non-sensitive sites)" \n')
     for cella in df['a'].values:
         if not isinstance(cella, str):
             break
         f.write(str(i) + " " + str(cella) + " 0 " + "\n")
         i = i + 1
 
-with open(OUTPUT_PATH,"a") as f:
-    i = 1
-    for cella , cellb , cellc in df[['a','b','c']].values:
-        if not isinstance(cella, str):
-            break
-        f.write(str(i) + " " + str(cella) + " 0 " + str(cellb) + " " + str(cellc) + "\n")
-        i = i + 1
 
-# for cell in df['a'].values:
-#     print(cell)
-# df[['a','b']].to_csv(path_or_buf=RESULT_PATH,index=True,sep=" ", header=False,mode="w")
+# Deep learning result makes this output.txt
+# with open(OUTPUT_PATH,"a") as f:
+#     i = 1
+#     for cella , cellb , cellc in df[['a','b','c']].values:
+#         if not isinstance(cella, str):
+#             break
+#         f.write(str(i) + " " + str(cella) + " 0 " + str(cellb) + " " + str(cellc) + "\n")
+#         i = i + 1
+
+
 
